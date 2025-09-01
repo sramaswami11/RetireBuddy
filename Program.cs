82e723d2cebfdef5e05cd1225013f9ba56909165
Siva Ramaswami <sramaswami11@gmail.com>
2025-08-31 21:57:57 -0400

    Fix culture to enforce en-US $ formatting

---
+++ Program.cs
@@ -0,0 +1,41 @@
+using RetireBuddy.Components;
+using RetireBuddy.Context;
+using RetireBuddy.Services;
+using System.Globalization;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// ? Force US culture for correct $ formatting
+var cultureInfo = new CultureInfo("en-US");
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents();
+
+// Register planning components
+builder.Services.AddScoped<PlanningContext>();
+builder.Services.AddScoped<TaxCalculationService>();
+builder.Services.AddScoped<SocialSecurityService>();
+builder.Services.AddScoped<IrmaaService>();
+builder.Services.AddScoped<PlanningProtocol>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+
+app.UseAntiforgery();
+
+app.MapStaticAssets();
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode();
+
+app.Run();
